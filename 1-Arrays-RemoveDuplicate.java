3a60ef6f902dac96818b654f03dc4d42121f100b
amjad2019 <35986725+amjad2019@users.noreply.github.com>
2026-04-15 16:44:44 +0300

    Add files via upload

---
+++ 1-Arrays-RemoveDuplicate.java
@@ -0,0 +1,23 @@
+/*
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+Consider the number of unique elements in nums to be k. After removing duplicates, return the number of unique elements k.
+The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
+*/
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        if(nums.length == 0){
+            return 0;
+        }
+            int uniqueIndex = 0;
+            
+            for(int i = 0; i<nums.length; i++){
+                if(nums[i] != nums[uniqueIndex]){
+                    uniqueIndex ++;
+                    nums[uniqueIndex] = nums[i];
+                }
+            }  
+        return uniqueIndex +1;        
+    }
+}
